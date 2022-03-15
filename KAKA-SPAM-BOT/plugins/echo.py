6be7f4190080c87200785f55f3b30beb23dac916
Kaka026 <96742314+Kaka026@users.noreply.github.com>
2022-03-15 16:20:14 +0530

    Update echo.py

--- KAKA-SPAM-BOT/plugins/echo.py
+++ KAKA-SPAM-BOT/plugins/echo.py
@@ -6,11 +6,11 @@ import requests
 from telethon import events
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 
-from RiZoeLXSpam import Riz, Riz2, Riz3, Riz4, Riz5 , Riz6, Riz7, Riz8, Riz9, Riz10, SUDO_USERS, OWNER_ID
+from KAKA-SPAM-BOT import Riz, Riz2, Riz3, Riz4, Riz5 , Riz6, Riz7, Riz8, Riz9, Riz10, SUDO_USERS, OWNER_ID
 
-from RiZoeLXSpam import CMD_HNDLR as hl
-from RiZoeLXSpam.sql.echo_sql import addecho, get_all_echos, is_echo, remove_echo
-from resources.data import RiZoeLX
+from KAKA-SPAM-BOT import CMD_HNDLR as hl
+from KAKA-SPAM-BOT.sql.echo_sql import addecho, get_all_echos, is_echo, remove_echo
+from resources.data import kaka
 
 
 @Riz.on(events.NewMessage(incoming=True, pattern=r"\%saddecho(?: |$)(.*)" % hl))
