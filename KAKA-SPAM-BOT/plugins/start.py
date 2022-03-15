58ba10865b51cf2dfdab70933c654a6471da2bb2
Kaka026 <96742314+Kaka026@users.noreply.github.com>
2022-03-15 16:41:53 +0530

    Update start.py

--- KAKA-SPAM-BOT/plugins/start.py
+++ KAKA-SPAM-BOT/plugins/start.py
@@ -4,27 +4,27 @@ from telethon.tl.functions.users import GetFullUserRequest
 from telethon import events, Button
 from telethon.tl.custom import button
 from .. import Riz, Riz2, Riz3, Riz4, Riz5, Riz6, Riz7, Riz8, Riz9, Riz10, ALIVE_PIC, OWNER_ID
-from RiZoeLXSpam.plugins.help import *
+from KAKA-SPAM-BOT.plugins.help import *
 
 
-RIZ_IMG = ALIVE_PIC if ALIVE_PIC else "https://telegra.ph/file/ba87c58f01a6fcb5ef512.jpg"
+KAKA_IMG = ALIVE_PIC if ALIVE_PIC else "https://te.legra.ph/file/90e8146c356459a0db464.jpg"
 
-Riz_Button = [
+KAKA_Button = [
         [
-        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/DNHxHELL")
+        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/marrkmusic")
         ],
         [
         Button.inline("• ᴄᴍᴅs •", data="help_back")
         ]
         ]
                
-RizX_Button = [
+KAKA_Button = [
         [
-        Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/RiZoeLX"),
-        Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/DNHxHELL")
+        Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/marrkchannel"),
+        Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/marrkmusic")
         ],
         [
-        Button.url("• ʀᴇᴘᴏ •", "https://github.com/MrRizoel/RiZoeLXSpam")
+        Button.url("• ʀᴇᴘᴏ •", "https://github.com/Kaka026/KakaSpamBot")
         ]
         ]
         
@@ -55,13 +55,13 @@ async def start(event):
        usermsg = f"**Hello, {firstname} ! Nice To Meet You, Well I Am {bot_id}, An Powerfull Spam Bot.** \n\n**If You Want Your Own Spam Bots You Can Deploy From Button Below.** \n\n**𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 [𝐑𝐈𝐙𝐎𝐄𝐋 𝐗](https://t.me/RiZoeLX)**"
        if event.sender_id == OWNER_ID:
             await event.client.send_file(TheRiZoeL,
-                  RIZ_IMG,
+                  KAKA_IMG,
                   caption=ownermsg, 
-                  buttons=Riz_Button)
+                  buttons=KAKA_Button)
        else:
             await event.client.send_file(TheRiZoeL,
-                  RIZ_IMG,
+                  KAKA_IMG,
                   caption=usermsg, 
-                  buttons=RizX_Button)
+                  buttons=KAKA_Button)
                 
 
